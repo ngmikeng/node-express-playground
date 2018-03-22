b6f00aae4a76e35245ced0954860dfcaeb1834c4
Minh Nguyen <ngmikeng@gmail.com>
2018-03-22 18:36:45 +0700

    Initial, play with tinycolor and tinygradient, classify color by range min and max.

---
+++ controllers/color.js
@@ -0,0 +1,40 @@
+const tinycolor = require('tinycolor2');
+const tinygradient = require('tinygradient');
+
+function ColorsRatio(arrayColors, colorLength, minValue, maxValue) {
+	this.ratio = 1;
+	this.minValue = minValue || 0;
+	this.maxValue = maxValue || 100;
+	this.colorsRatio = _getRatioColorsByGradient(arrayColors, colorLength);
+}
+
+ColorsRatio.prototype.calculateRatio = function(inputValue) {
+	if (inputValue >= this.minValue && inputValue <= this.maxValue) {
+		return (inputValue - this.minValue) / (this.maxValue - this.minValue);
+	}
+	return;
+}
+
+ColorsRatio.prototype.findColorByRatio = function(ratio) {
+	if (ratio) {
+		return this.colorsRatio.find((color) => ratio >= color.minRatio && ratio <= color.maxRatio);
+	}
+	return;
+}
+
+function _getRatioColorsByGradient(arrayColors = ['#000', '#cdcdcd', '#fff'], colorLength = 10) {
+	let gradient = tinygradient(arrayColors);
+	let colors = [];
+
+	gradient.rgb(colorLength).forEach((color, index, array) => {
+		colors.push({
+			minRatio: index / array.length,
+			maxRatio: (index + 1) / array.length,
+			color: color.toHslString()
+		});
+	});
+
+	return colors;
+}
+
+module.exports = { ColorsRatio: ColorsRatio };
---
+++ routes/colors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const tinycolor = require('tinycolor2');
+const tinygradient = require('tinygradient');
+const ColorsRatio = require('../controllers/color').ColorsRatio;
+
+function getRatioGradient(arrayColors) {
+    var gradient = tinygradient(arrayColors);
+    var stops = [];
+    gradient.rgb(10).forEach(function(color, index, array) {
+        stops.push([
+            (index + 1) / 10,
+            color.toHslString()
+        ]);
+    });
+
+    return stops;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	let colorsRatio = new ColorsRatio(['#000', '#cdcdcd', '#fff'], 100, 0, 1000);
+	let data = [];
+
+	for (let i = 0; i < 100; i++) {
+		data.push({ value:getRandomArbitrary(0, 1000) });
+	}
+
+	data = data.map((item) => {
+		let ratio = colorsRatio.calculateRatio(item.value);
+		let objColor = colorsRatio.findColorByRatio(ratio);
+		if (objColor && objColor.color) {
+			item.color = objColor.color;
+		}
+		return item;
+	});
+	
+	console.log(data);
+  	res.render('pages/color', { title: 'Color', data: data });
+});
+
+module.exports = router;
