43931393a66dd49e29ae3e0fed73566dae4c6db5
Minh Nguyen <ngmikeng@gmail.com>
2018-04-04 13:58:19 +0700

    Add gulp task to copy dependencies to public/vendor.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# node-express-playground
+node-express-playground
+===
 Playground for practice, testing, play with nodejs expressjs.
+* Multiple page app.
+* Use pug template engine.
 
 ## Quick start
+Install dependencies and setup the vendor folder.
+```js
+npm install
+npm run vendor
+```
 Run without nodemon.
 ```js
 npm start
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+
+// Copy third party libraries from /node_modules into ./public/vendor
+gulp.task('vendor', function() {
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./public/vendor/bootstrap'))
+
+  // Font Awesome
+  gulp.src([
+      './node_modules/font-awesome/**/*',
+      '!./node_modules/font-awesome/{less,less/*}',
+      '!./node_modules/font-awesome/{scss,scss/*}',
+      '!./node_modules/font-awesome/.*',
+      '!./node_modules/font-awesome/*.{txt,json,md}'
+    ])
+    .pipe(gulp.dest('./public/vendor/font-awesome'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./public/vendor/jquery'))
+
+  // jQuery Easing
+  gulp.src([
+      './node_modules/jquery.easing/*.js'
+    ])
+    .pipe(gulp.dest('./public/vendor/jquery-easing'))
+});
+
+// Default task
+gulp.task('default', ['vendor']);
