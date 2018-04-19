9ded2093d0a7dfca368086341d27c6c433076b58
Minh Nguyen <ngmikeng@gmail.com>
2018-04-19 18:10:53 +0700

    Fix sidebar links, WIP simple chat playground.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var usersRouter = require('./routes/users');
 var colorsRouter = require('./routes/colors');
+var simpleChatRouter = require('./routes/simpleChat');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/users', usersRouter);
 app.use('/colors', colorsRouter);
+app.use('/simpleChat', simpleChatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
